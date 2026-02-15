5194bd78aedad375bc688c4d9f94080c11aa33bf
Ben <meowcat@meowcat.site>
2026-02-15 10:09:26 +0000

    base commit

---
+++ src/main/java/site/meowcat/springcatgames/SpringCatGamesApplication.java
@@ -0,0 +1,13 @@
+package site.meowcat.springcatgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCatGamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCatGamesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/meowcat/springcatgames/SpringCatGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package site.meowcat.springcatgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCatGamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
